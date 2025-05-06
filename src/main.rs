e0f16896fd0b89f6c8bdc2b576bbda2e5ea33f13
Diesel300300 <joran.verbruggen@icloud.com>
2025-05-06 22:17:25 +0200

    basic file reading and counting nodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::io::{BufReader};
+use quick_xml::Reader;
+use quick_xml::events::Event;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./data/map").expect("Couldn't open map file");
+    
+    let reader = BufReader::new(file);  
+    
+    let mut xml = Reader::from_reader(reader);
+    xml.trim_text(true);
+    
+    let mut buf = Vec::new();
+    let mut count = 0_usize;
+
+    loop {
+        match xml.read_event_into(&mut buf) {
+            Ok(Event::Start(ref e)) | Ok(Event::Empty(ref e)) if e.name().0 == b"node" => {
+                count += 1;
+            },
+            Ok(Event::Eof) => break,
+            Ok(_) => (),
+            Err(e) => panic!("XML error: {}", e)
+        };
+        buf.clear();
+    }
+
+    println!("There are {} amount of nodes in the file", count);
 }
