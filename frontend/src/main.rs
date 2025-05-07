f93a82551c5c6abd8898e7165a39a0a1b642bf7e
Diesel300300 <joran.verbruggen@icloud.com>
2025-05-08 01:16:25 +0200

    file structure with tailwindcss

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,20 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
 fn main() {
-    println!("Hello, world!");
+    dioxus::launch(App);
 }
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        div { class: "bg-red-100", "Hello world!" }
+    }
+}
+
